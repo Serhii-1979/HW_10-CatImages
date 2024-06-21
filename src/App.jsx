24cbe2765d3946fe2861873679d6f8932d9d12ec
Serhii-1979 <krasovskyi76@gmail.com>
2024-06-21 18:41:24 +0200

    Deleted files

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,10 @@
 import React from 'react';
-// import UserProfile from './HW_6/UserProfile'
 import './App.css';
-// import SpotifyList from './components/SpotifyList';
-// import Counter from './testing/Counter'
-// import ListItems from './components/ListItems';
-// import CatImage from './HW_9/CatImage'
 import PhoneCont from './HW_9/PhoneCont'
 
 function App() {
   return (
     <div className="App">
-      {/* <SpotifyList /> */}
-      {/* <UserProfile /> */}
-      {/* <Counter /> */}
-      {/* <ListItems /> */}
-      {/* <CatImage /> */}
       <PhoneCont />
     </div>
   );
